b233ebdd3916c9da3e9cd440ff97a8b4f1c49e4f
lucasponteslpa <lpa@cin.ufpe.br>
2020-11-17 10:26:05 -0300

    Fix data processing

--- dataset.py
+++ dataset.py
@@ -3,6 +3,7 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn import datasets
 from sklearn.decomposition import PCA
 import numpy as np
+from pdb import set_trace
 
 class ProcessData():
 
@@ -13,16 +14,15 @@ class ProcessData():
         self.Y = self.iris.target
 
         self.mean = np.array([np.mean(self.X[:,i]) for i in range(self.X.shape[1])])
-        self.var = np.array([np.var(self.X[:,i]) for i in range(self.X.shape[1])])
+        self.var = np.array([np.sqrt(np.var(self.X[:,i])) for i in range(self.X.shape[1])])
         center = (self.X - np.reshape(self.mean,(1,2)))/np.reshape(self.var,(1,2))
         a = np.array([np.linalg.norm(center[i,:]) for i in range(self.X.shape[0])])
-        a = np.array([a,a])
-        self.norm = center/np.reshape(a,(a.shape[1],a.shape[0]))
-        #self.norm = self.X/np.reshape(a,(a.shape[1],a.shape[0]))
+        #set_trace()
+        self.norm = np.zeros((self.X.shape[0],self.X.shape[1]))
+        for i in range(self.X.shape[0]):
+            self.norm[i,:] = center[i,:]/a[i]
 
     def show_data(self):
-        x_min, x_max = self.norm[:, 0].min() - .5, self.norm[:, 0].max() + .5
-        y_min, y_max = self.norm[:, 1].min() - .5, self.norm[:, 1].max() + .5
 
         plt.figure(2, figsize=(8, 6))
         plt.clf()
@@ -32,23 +32,7 @@ class ProcessData():
         plt.xlabel('Sepal length')
         plt.ylabel('Sepal width')
 
-        plt.xlim(-1, 1)
-        plt.ylim(-1, 1)
-        plt.xticks(())
-        plt.yticks(())
-
-        # To getter a better understanding of interaction of the dimensions
-        # plot the first three PCA dimensions
-        fig = plt.figure(1, figsize=(8, 6))
-        ax = Axes3D(fig, elev=-150, azim=110)
-        X_reduced = PCA(n_components=3).fit_transform(self.iris.data)
-        ax.scatter(X_reduced[:, 0], X_reduced[:, 1], X_reduced[:, 2], c=self.Y, s=40)
-        ax.set_title("First three PCA directions")
-        ax.set_xlabel("1st eigenvector")
-        ax.w_xaxis.set_ticklabels([])
-        ax.set_ylabel("2nd eigenvector")
-        ax.w_yaxis.set_ticklabels([])
-        ax.set_zlabel("3rd eigenvector")
-        ax.w_zaxis.set_ticklabels([])
+        plt.xlim(-1.5, 1.5)
+        plt.ylim(-1.5, 1.5)
 
         plt.show()
