a657378c2782ce62f8141690f7bbca94a49bbcd4
lucasponteslpa <lpa@cin.ufpe.br>
2020-11-17 10:52:43 -0300

    Add gates and minimum quantum classifier

---
+++ minimum.py
@@ -0,0 +1,11 @@
+import qiskit
+import numpy as np
+from gates import Rz, Ry
+from qiskit.quantum_info.operators import Operator
+
+class MinimumQClassifier():
+
+    def __init__(self):
+        circuito = qiskit.QuantumCircuit(4)
+
+    def construct(self):
