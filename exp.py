b3023c7ddb94f2d1575c986c8bf2d61de7b4502a
lucasponteslpa <lpa@cin.ufpe.br>
2020-11-22 19:50:56 -0300

    Put argparser

--- exp.py
+++ exp.py
@@ -18,19 +18,33 @@ def print_res(cq):
     answer = results.get_counts()
     print(answer)
 
+def run_classifier(params):
+    dataexp = ProcessData()
+    #dataexp.show_data()
+    if(params["circuit"]=="minimun"):
+        mini = MinimumQClassifier()
+        print(mini.circuito)
+        print_res(mini.circuito)
+    #set_trace()
+    else:
+        # qclass = QClassifier(dataexp.norm[42:58,:],dataexp.Y[42:58], dataexp.norm[54,:])
+        qclass = QClassifier(np.append(dataexp.norm[0:32,:],dataexp.norm[55:87,:]).reshape(64,2),np.append(dataexp.Y[0:32],dataexp.Y[55:87]), dataexp.norm[32,:])
+        # qclass = QClassifier(np.array([[0.0,1.0],[0.789,0.615]]), np.array([0,1]), np.array([-0.549,0.836]))
+        qclass.preparation()
+        #print(qclass.circuito)
+        #print_state(qclass.circuito)
+        print_res(qclass.circuito)
 
-dataexp = ProcessData()
-#dataexp.show_data()
+if __name__ == '__main__':
+    import argparse
 
-# mini = MinimumQClassifier()
-# print(mini.circuito)
-# print_state(mini.circuito)
-#set_trace()
+    parser = argparse.ArgumentParser(description='Preprocess')
+    parser.add_argument('--circuit', type=str, help='Define what circuit will be used')
+    parser.add_argument('--num_exemples', type=str, help='Number of training exemples')
+    parser.add_argument('--test_index', type=str, help='Index of test data')
 
-# qclass = QClassifier(dataexp.norm[42:58,:],dataexp.Y[42:58], dataexp.norm[54,:])
-qclass = QClassifier(np.append(dataexp.norm[0:32,:],dataexp.norm[55:87,:]).reshape(64,2),np.append(dataexp.Y[0:32],dataexp.Y[55:87]), dataexp.norm[32,:])
-# qclass = QClassifier(np.array([[0.0,1.0],[0.789,0.615]]), np.array([0,1]), np.array([-0.549,0.836]))
-qclass.preparation()
-#print(qclass.circuito)
-#print_state(qclass.circuito)
-print_res(qclass.circuito)
+    params = vars(parser.parse_args())
+
+    print('PARAMS:')
+    print(params)
+    run_classifier(params)
